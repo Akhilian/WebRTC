23343a9dca9309aa0e0e9416b66aed26747182fb
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-04-07 09:42:25 +0200

    :heavy_plus_sign: GH-1 | Add Bulma as CSS framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Call from './Call';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <section class="section">
+      <div className="container">
+        <div class="columns">
+          <div class="column">
+            <Call playerName="Jjpaal" />
+          </div>
+          <div class="column">
+            <Call playerName="Akhilian" />
+          </div>
+        </div>
+      </div>
+      </section>
+    );
+  }
 }
 
 export default App;
